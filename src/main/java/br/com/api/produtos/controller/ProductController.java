b296bb12ab54773a0e46f10be1fc33ce702bc904
Fernando <silentguyband@gmail.com>
2024-12-14 16:59:17 -0300

    Configure Controller

--- src/main/java/br/com/api/produtos/controller/ProductController.java
+++ src/main/java/br/com/api/produtos/controller/ProductController.java
@@ -1,5 +1,12 @@
 package br.com.api.produtos.controller;
 
-public class ProductController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ProductController {
+    @GetMapping("/")
+    public String rota(){
+        return "API de produtos funcionando!";
+    }
 }
