0fc8ac6ea85c19069b7ff2bc5f3ddd92dd5f17ca
Fernando <silentguyband@gmail.com>
2024-12-14 17:21:19 -0300

    Route to list products

--- src/main/java/br/com/api/produtos/controller/ProductController.java
+++ src/main/java/br/com/api/produtos/controller/ProductController.java
@@ -1,10 +1,22 @@
 package br.com.api.produtos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.produtos.models.ModelProduct;
+import br.com.api.produtos.service.ProductService;
+
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductService ps;
+
+    @GetMapping("/listar")
+    public Iterable<ModelProduct> listar(){
+        return ps.listar();
+    }
+    
     @GetMapping("/")
     public String rota(){
         return "API de produtos funcionando!";
