eccf5944dfb377dd5d96b8ace61b8a6e5a918288
Fernando <silentguyband@gmail.com>
2024-12-19 15:28:30 -0300

    Route to change products

--- src/main/java/br/com/api/produtos/controller/ProductController.java
+++ src/main/java/br/com/api/produtos/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ import br.com.api.produtos.service.ProductService;
 public class ProductController {
     @Autowired
     private ProductService ps;
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alter(@RequestBody ModelProduct pm){
+        return ps.registeralter(pm, "alterar");
+    }
 
     @PostMapping("/cadastrar")
     public ResponseEntity<?> register(@RequestBody ModelProduct pm){
