b2d16a7cd0abfaec3d6303fdeef4ff67cb8a22a9
Fernando <silentguyband@gmail.com>
2024-12-19 15:55:06 -0300

    Service delete product

--- src/main/java/br/com/api/produtos/controller/ProductController.java
+++ src/main/java/br/com/api/produtos/controller/ProductController.java
@@ -2,18 +2,26 @@ package br.com.api.produtos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.models.ModelProduct;
+import br.com.api.produtos.models.ModelResponse;
 import br.com.api.produtos.service.ProductService;
 @RestController
 public class ProductController {
     @Autowired
     private ProductService ps;
+
+    @DeleteMapping("remover/{codigo}")
+    public ResponseEntity<ModelResponse> remove(@PathVariable long codigo){
+        return ps.remove(codigo);
+    }
 
     @PutMapping("/alterar")
     public ResponseEntity<?> alter(@RequestBody ModelProduct pm){
